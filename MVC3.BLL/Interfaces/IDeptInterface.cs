570985c9e1ee748955f5e4fcc0443b4960cab6ed
saramotaha <saramohamedtaha909@gmail.com>
2024-03-21 04:11:49 -0700

    Business Logic Layer

---
+++ MVC3.BLL/Interfaces/IDeptInterface.cs
@@ -0,0 +1,24 @@
+﻿using MVC3.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC3.BLL.Interfaces
+{
+    internal interface IDeptInterface
+    {
+
+        IEnumerable<Department> GetAll();
+        Department GetById(int id);
+        int Add(Department d);
+        int Update(Department d);
+        int Delete(Department d);
+
+
+
+
+
+    }
+}
---
+++ MVC3.BLL/Repos/DeptRepo.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC3.BLL.Interfaces;
+using MVC3.DAL.Data;
+using MVC3.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC3.BLL.Repos
+{
+    internal class DeptRepo : IDeptInterface
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DeptRepo(AppDbContext  appDbContext)
+        {
+
+            _appDbContext = appDbContext;
+            
+        }
+        public int Add(Department d)
+        {
+            _appDbContext.Departments.Add(d);
+
+            return (_appDbContext.SaveChanges());
+        }
+
+        public int Delete(Department d)
+        {
+            _appDbContext.Departments.Remove(d);
+            return (_appDbContext.SaveChanges());
+
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _appDbContext.Departments.AsNoTracking().ToList();
+        }
+
+        public Department GetById(int id)
+        {
+            //var dept = _appDbContext.Departments.Local.Where(d => d.Id == id).FirstOrDefault();
+            //if(dept == null)
+            //{
+            //    dept = _appDbContext.Departments.Where(d => d.Id == id).FirstOrDefault();
+            //}
+
+            //return dept;
+
+
+
+            return _appDbContext.Departments.Find(id);
+        }
+
+        public int Update(Department d)
+        {
+            _appDbContext.Departments.Update(d);
+            return (_appDbContext.SaveChanges());
+        }
+    }
+}
